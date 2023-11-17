54c082823f7980b903405b4dad60f26bf2a9199f
Oskar Korczak <oskar.korczak@protonmail.com>
2023-11-17 01:41:49 +0100

    feat: Refactor translation service to include token count tracking

--- translator/translate_service.py
+++ translator/translate_service.py
@@ -6,38 +6,48 @@ from translator.pricing import calculate_cost
 from openai.types.chat import ChatCompletion, ChatCompletionMessage
 import logging
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Message(BaseModel):
     content: str
     role: str
-
-    @classmethod
-    def from_chat_completion_message(cls, message: ChatCompletionMessage):
-        return cls(content=message.content, role=message.role)
+    token_count: int = Field(default=0, exclude=True)
 
 
 def create_message(text):
     return Message(content=text, role="user")
 
 
+def sum_tokens(messages: list[Message]) -> int:
+    return sum([message.token_count for message in messages])
+
+
 class TranslateService:
     def __init__(self, client: OpenAI):
         self.client = client
         self.total_cost = Decimal("0.00")
-        self.messages = [
-            Message(content=PERSONALITY, role="system"),
-        ]
+        self.messages = [Message(content=PERSONALITY, role="system")]
         self.model = "gpt-3.5-turbo-1106"
+
+    def get_messages_dicts(self) -> list[dict[str, str]]:
+        return [message.model_dump() for message in self.messages]
 
     def translate(self, text: str) -> str:
         self.messages.append(create_message(text))
         response = self.client.chat.completions.create(
-            model=self.model, messages=self.messages
+            model=self.model, messages=self.get_messages_dicts()
         )
+        input_tokens = response.usage.prompt_tokens
+        self.messages[-1].token_count = input_tokens - sum_tokens(self.messages)
+
+        message = response.choices[0].message
         self.messages.append(
-            Message.from_chat_completion_message(response.choices[0].message)
+            Message(
+                content=message.content,
+                role=message.role,
+                token_count=response.usage.completion_tokens,
+            )
         )
         translated = response.choices[0].message.content
         self.report_tokens(response)
@@ -57,3 +67,6 @@ class TranslateService:
         input_tokens = response.usage.prompt_tokens
         output_tokens = response.usage.completion_tokens
         logging.info(f"Tokens: {input_tokens} input, {output_tokens} output")
+
+        for message in self.messages:
+            logging.info(f"Tokens: {message.token_count} {message.role}")
